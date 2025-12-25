74baf12145c97ef11a9850f501c0d42a43f31f32
ales2026 <ale.urankar@gmail.com>
2025-12-25 20:46:16 +0100

    writing and reading json files

--- C++_project/Main.cpp
+++ C++_project/Main.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
-#include <string>
+#include <fstream>
+#include "json.hpp"
 
+
+using json = nlohmann::json;
+
+void Write()
+{
+	json j; 
+	
+	j["one"] = "sentence one";
+	j["two"] = { "sentence", 1 };
+	j["two"] = { "sentence", 1, 3 };
+
+	std::ofstream file("output.json");
+	file << j.dump(4);
+}
+
+void Read()
+{
+	std::ifstream file("output.json");
+	json j;
+
+	file >> j;
+	
+	std::cout << j["one"] << "\n";
+	std::cout << j.dump(4) << "\n";
+}
 
 int main()
 {
+	Read();
+
+
 	return 0;
 }
