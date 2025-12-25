a046dc053422a15184ffcec04c08dcb7c16bf24b
ales2026 <ale.urankar@gmail.com>
2025-12-25 20:15:59 +0100

    importing nlohman json

--- C++_project/Calculator.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class Calculator {
-public:
-	int Sum(int a, int b);
-	double Sum(double a, double b);
-private:
-};
--- C++_project/Main.cpp
+++ C++_project/Main.cpp
@@ -1,37 +1,8 @@
-#include "MyClass.h"
-#include "Calculator.h"
-#include "NumGuess.h"
-#include "Account.h"
 #include <iostream>
-#include <vector>
+#include <string>
 
-int main() {
-	Account ac;
-	char choice;
-	int quantity;
-	while (true)
-	{
-		std::cout << "(b)alance, (d)eposit, (w)ithdrawl\n";
-		std::cin >> choice;
-		switch (choice)
-		{
-		case 'b':
-			ac.Balance();
-			break;
-		case 'd':
-			std::cout << "how much you want to deposit?\n";
-			std::cin >> quantity;
-			ac.Deposit(quantity);
-			break;
-		case 'w':
-			std::cout << "how much do you want to withdrawl?\n";
-			std::cin >> quantity;
-			ac.Withdrawl(quantity);
-			break;
-		default:
-			std::cout << "choose again\n";
-		}
-	}
 
+int main()
+{
 	return 0;
 }
