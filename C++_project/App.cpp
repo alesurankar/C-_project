489219f74a3570303df65b260a9ee7f1e798757d
ales2026 <ale.urankar@gmail.com>
2026-01-10 15:15:36 +0100

    more threads to do the heavy work in paralel

--- C++_project/App.cpp
+++ C++_project/App.cpp
@@ -1,6 +1,9 @@
 #include "App.h"
 #include <iostream>
 
+
+using Clock = std::chrono::high_resolution_clock;
+
 App::App(std::atomic<bool>& running_in)
 	:
 	running(running_in)
@@ -16,15 +19,46 @@ void heavyWork()
 
 void App::Go()
 {
-	float dt = ft.Mark();
+	auto frameStart = Clock::now();
+
 	if (i >= 5) {
 		running = false;
 	}
-	//heavyWork();
 	std::cout << "number: " << i << "\n";
-	if (i > 0) {
-		std::cout << "cycle time: " << dt << "\n";
-	}
+
+	float w2Time;
+	float w4Time;
+	std::thread t1([&w2Time, this]() {
+		w2Time = work();
+		});
+	std::thread t2([&w4Time, this]() {
+		w4Time = work();
+		});
+	float w1Time = work();
+	float w3Time = work();
+
+	t1.join();
+	t2.join();
+
+	auto frameEnd = Clock::now();
+
+	auto frameTime = std::chrono::duration<double, std::milli>(frameEnd - frameStart).count();
+
+	std::cout << "work1 time: " << w1Time << " ms\n";
+	std::cout << "work2 time: " << w2Time << " ms\n";
+	std::cout << "work2 time: " << w3Time << " ms\n";
+	std::cout << "work2 time: " << w4Time << " ms\n";
+	std::cout << "total cycle time: " << frameTime << " ms\n\n";
+
 	std::cout << "\n";
 	i++;
+}
+
+float App::work()
+{
+	auto start = Clock::now();
+	heavyWork();
+	auto end = Clock::now();
+
+	return std::chrono::duration<float, std::milli>(end - start).count();
 }
--- C++_project/App.h
+++ C++_project/App.h
@@ -1,14 +1,15 @@
 #pragma once
 #include <atomic>
 #include "FrameTimer.h"
+#include <thread>
 
 class App 
 {
 public:
 	App(std::atomic<bool>& running_in);
 	void Go();
+	float work();
 private:
 	int i = 0;
 	std::atomic<bool>& running;
-	FrameTimer ft;
 };
