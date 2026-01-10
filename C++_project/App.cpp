1ccf16c47a9c12e8c5b380115e2ed0c1fd17ba06
ales2026 <ale.urankar@gmail.com>
2026-01-10 17:23:51 +0100

    spawning threads in a vector

--- C++_project/App.cpp
+++ C++_project/App.cpp
@@ -7,13 +7,12 @@ using Clock = std::chrono::high_resolution_clock;
 App::App(std::atomic<bool>& running_in)
 	:
 	running(running_in)
-{
-}
+{}
 
 void heavyWork()
 {
 	volatile double x = 0;
-	for (int i = 0; i < 50'000'000; ++i)
+	for (int i = 0; i < 20'000'000; ++i)
 		x += std::sin(i);
 }
 
@@ -23,79 +22,48 @@ void App::Go()
 
 	if (i >= 5) {
 		running = false;
+		unsigned int nCores = std::thread::hardware_concurrency();
+		std::cout << "CPU cores: " << nCores << "\n";
+		
 	}
 	std::cout << "number: " << i << "\n";
 
-	float w1Time;
-	std::thread t1([&w1Time, this]() {
-		w1Time = work();
-		});
+	for (int i = 0; i < threadsNum; i++) {
+		threads.emplace_back([this]() {
+			float time = work();
+			std::lock_guard<std::mutex> lock(mtx);
+			results.emplace_back(time);
+			});
+	}
 
-	float w2Time;
-	std::thread t2([&w2Time, this]() {
-		w2Time = work();
-		});
+	float w1Time = work();
+	float w2Time = work();
 
-	float w5Time;
-	std::thread t5([&w5Time, this]() {
-		w5Time = work();
-		});
-
-	float w6Time;
-	std::thread t6([&w6Time, this]() {
-		w6Time = work();
-		});
-
-	float w7Time;
-	std::thread t7([&w7Time, this]() {
-		w7Time = work();
-		});
-
-	float w8Time;
-	std::thread t8([&w8Time, this]() {
-		w8Time = work();
-		});
-
-	float w9Time;
-	std::thread t9([&w9Time, this]() {
-		w9Time = work();
-		});
-
-	float w10Time;
-	std::thread t10([&w10Time, this]() {
-		w10Time = work();
-		});
-
-	float w3Time = work();
-	float w4Time = work();
-
-
-	t1.join();
-	t2.join();
-	t5.join();
-	t6.join();
-	t7.join();
-	t8.join();
-	t9.join();
-	t10.join();
+	for (auto& t : threads) {
+		t.join();
+	}
 
 	auto frameEnd = Clock::now();
 
 	auto frameTime = std::chrono::duration<double, std::milli>(frameEnd - frameStart).count();
 
-	std::cout << "work1 time: " << w1Time << " ms\n";
-	std::cout << "work2 time: " << w2Time << " ms\n";
-	std::cout << "work3 time: " << w3Time << " ms\n";
-	std::cout << "work4 time: " << w4Time << " ms\n";
-	std::cout << "work5 time: " << w5Time << " ms\n";
-	std::cout << "work6 time: " << w6Time << " ms\n";
-	std::cout << "work7 time: " << w7Time << " ms\n";
-	std::cout << "work8 time: " << w8Time << " ms\n";
-	std::cout << "work9 time: " << w9Time << " ms\n";
-	std::cout << "work10 time: " << w10Time << " ms\n";
+
+	std::cout << "work1: " << w1Time << " ms\n";
+	std::cout << "work2: " << w2Time << " ms\n";
+	for (int i = 0; i < results.size(); i++) {
+		std::cout << "work" << i + 3 << ": " << results[i] << " ms\n";
+	}
+
 	std::cout << "total cycle time: " << frameTime << " ms\n\n";
+	float allTimes = 0;
+	for (auto& t : times) {
+		allTimes += t;
+	}
+	std::cout << "total work time: " << allTimes << "\n";
 
 	std::cout << "\n";
+	threads.clear();
+	results.clear();
 	i++;
 }
 
@@ -103,7 +71,16 @@ float App::work()
 {
 	auto start = Clock::now();
 	heavyWork();
-	auto end = Clock::now();
+	auto endWork = Clock::now();
 
+	auto time = std::chrono::duration<float, std::milli>(endWork - start).count();
+	writeTime(time);
+	auto end = Clock::now();
 	return std::chrono::duration<float, std::milli>(end - start).count();
+}
+
+void App::writeTime(float time)
+{
+	std::lock_guard<std::mutex> lock(mtx);
+	times.emplace_back(time);
 }
--- C++_project/App.h
+++ C++_project/App.h
@@ -2,6 +2,8 @@
 #include <atomic>
 #include "FrameTimer.h"
 #include <thread>
+#include <vector>
+#include <mutex>
 
 class App 
 {
@@ -9,7 +11,13 @@ public:
 	App(std::atomic<bool>& running_in);
 	void Go();
 	float work();
+	void writeTime(float time);
 private:
 	int i = 0;
+	int threadsNum = 10;
 	std::atomic<bool>& running;
+	std::vector<float> times;
+	std::vector<std::thread> threads;
+	std::vector<float> results;
+	std::mutex mtx;
 };
