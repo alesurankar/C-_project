e9334d0ff44fe6ff13da639224839aaa02db6bf2
ales2026 <ale.urankar@gmail.com>
2026-01-10 15:32:02 +0100

    lots of multithreading

--- C++_project/App.cpp
+++ C++_project/App.cpp
@@ -26,19 +26,58 @@ void App::Go()
 	}
 	std::cout << "number: " << i << "\n";
 
+	float w1Time;
+	std::thread t1([&w1Time, this]() {
+		w1Time = work();
+		});
+
 	float w2Time;
-	float w4Time;
-	std::thread t1([&w2Time, this]() {
+	std::thread t2([&w2Time, this]() {
 		w2Time = work();
 		});
-	std::thread t2([&w4Time, this]() {
-		w4Time = work();
+
+	float w5Time;
+	std::thread t5([&w5Time, this]() {
+		w5Time = work();
 		});
-	float w1Time = work();
+
+	float w6Time;
+	std::thread t6([&w6Time, this]() {
+		w6Time = work();
+		});
+
+	float w7Time;
+	std::thread t7([&w7Time, this]() {
+		w7Time = work();
+		});
+
+	float w8Time;
+	std::thread t8([&w8Time, this]() {
+		w8Time = work();
+		});
+
+	float w9Time;
+	std::thread t9([&w9Time, this]() {
+		w9Time = work();
+		});
+
+	float w10Time;
+	std::thread t10([&w10Time, this]() {
+		w10Time = work();
+		});
+
 	float w3Time = work();
+	float w4Time = work();
+
 
 	t1.join();
 	t2.join();
+	t5.join();
+	t6.join();
+	t7.join();
+	t8.join();
+	t9.join();
+	t10.join();
 
 	auto frameEnd = Clock::now();
 
@@ -46,8 +85,14 @@ void App::Go()
 
 	std::cout << "work1 time: " << w1Time << " ms\n";
 	std::cout << "work2 time: " << w2Time << " ms\n";
-	std::cout << "work2 time: " << w3Time << " ms\n";
-	std::cout << "work2 time: " << w4Time << " ms\n";
+	std::cout << "work3 time: " << w3Time << " ms\n";
+	std::cout << "work4 time: " << w4Time << " ms\n";
+	std::cout << "work5 time: " << w5Time << " ms\n";
+	std::cout << "work6 time: " << w6Time << " ms\n";
+	std::cout << "work7 time: " << w7Time << " ms\n";
+	std::cout << "work8 time: " << w8Time << " ms\n";
+	std::cout << "work9 time: " << w9Time << " ms\n";
+	std::cout << "work10 time: " << w10Time << " ms\n";
 	std::cout << "total cycle time: " << frameTime << " ms\n\n";
 
 	std::cout << "\n";
